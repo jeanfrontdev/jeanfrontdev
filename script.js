1a1a28dd11367cf12554d0a7d822d481e9e2aa27
webydev <102455180+jeanfrontdev@users.noreply.github.com>
2022-04-09 09:18:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+  document.querySelector('.cafe').src = anything;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
